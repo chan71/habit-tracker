c2a14ce2ff988d6113325f1752002b236c7897b6
Victoriakim <victoriakim048@gmail.com>
2025-02-04 01:44:23 +0000

    Restored to '60aaa72dad99d4ad2142bd1f2844e857b42f8299'
    Replit-Restored-To:60aaa72dad99d4ad2142bd1f2844e857b42f8299

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,10 +17,8 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <Router />
-        <Toaster />
-      </div>
+      <Router />
+      <Toaster />
     </QueryClientProvider>
   );
 }
